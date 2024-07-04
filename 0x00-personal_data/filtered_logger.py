e634111b4c5ed78b08ff6daccda72817cf061980
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 15:56:10 +0400

    filtered_logger.py - log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -14,3 +15,28 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(r'(?<={}=)(.*?)(?={})'.format(
             field, separator), redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """
+        This is the constructor function
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        this function modifies how format works and add filtering
+        """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super().format(record)
