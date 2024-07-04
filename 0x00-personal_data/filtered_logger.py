9e8a69ced108362064c97f6e61d1ff0a3d2bc715
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 16:19:46 +0400

    filtered_logger.py - log formatter: adding more type hints

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,20 +18,20 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 
 class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
     """
-    Redacting Formatter class
-    """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """this function modifies how format works and add filtering
+        """formats a LogRecord.
         """
-        msg = super().format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                                  msg, self.SEPARATOR)
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return super().format(record)
