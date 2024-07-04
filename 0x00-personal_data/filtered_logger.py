9ba1cb3bc9a12e67beb0d9ddeec9e56596acece8
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 15:17:37 +0400

    modifying type hint??

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,16 +5,13 @@
 import re
 from typing import List
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
-
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 seperator: str):
+                 seperator: str) -> str:
     """
     This function returns the log message obfuscated
     """
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, seperator), replace(redaction), message)
+    for field in fields:
+         regex = '(?<={}=)(.*?)(?={})'.format(field, seperator)
+         message = re.sub(regex, redaction, message)
+     return message
