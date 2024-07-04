6a724adb7b495de671e2ca201d2784ef01a942c2
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 15:24:44 +0400

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
 0. Regex-ing
+In the context of computer science, it's crucial to protect PII to maintain
+privacy and prevent identity theft. One way to do this is by filtering logs.
 """
 import re
 from typing import List
@@ -12,6 +14,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     This function returns the log message obfuscated
     """
     for field in fields:
-        regex = '(?<={}=)(.*?)(?={})'.format(field, seperator)
-        message = re.sub(regex, redaction, message)
+        message = re.sub(r'(?<={}=)(.*?)(?={})'.format(
+            field, seperator), redaction, message)
     return message
