4e92eaface6fd22b54ce0df002e7e790fc8ee4ae
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 17:10:15 +0400

    filtered_logger.py mypy compliant db name default

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -40,22 +40,20 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates a connector to a database.
     """
-    This function returns a connector to the database
-    """
-    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
-    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
-    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
-    name = os.environ.get("PERSONAL_DATA_DB_NAME", "holberton")
-
-    cnx = mysql.connector.connection.MySQLConnection(
-            user=username,
-            password=password,
-            port=3306,
-            host=host,
-            database=name
-        )
-    return cnx
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+        host=db_host,
+        port=3306,
+        user=db_user,
+        password=db_pwd,
+        database=db_name,
+    )
+    return connection
 
 
 class RedactingFormatter(logging.Formatter):
