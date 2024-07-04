7a5195b48366f71f85ac48242cf13c56b50cee52
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 15:33:21 +0400

    filtered_logger.py troubeshoot

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
+"""A module for filtering logs.
 """
-0. Regex-ing
-
-In the context of computer science, it's crucial to protect PII to maintain
-privacy and prevent identity theft. One way to do this is by filtering logs.
-"""
+import os
 import re
-from typing import List
 import logging
+import mysql.connector
+from typing import List
 
 
-def filter_datum(fields, redaction, message, seperator):
-    """ This function returns the log message obfuscated
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
     """
-    for field in fields:
-        message = re.sub(r'(?<={}=)(.*?)(?={})'.format(
-            field, seperator), redaction, message)
-    return message
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
