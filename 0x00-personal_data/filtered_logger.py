1cf634557a0c7298f5526feea707f514273bc28c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 15:35:55 +0400

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,17 +8,12 @@ import mysql.connector
 from typing import List
 
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     """Filters a log line.
     """
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+    for field in fields:
+        message = re.sub(r'(?<={}=)(.*?)(?={})'.format(
+            field, separator), redaction, message)
+    return message
