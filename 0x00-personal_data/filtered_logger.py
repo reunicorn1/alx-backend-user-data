9adef2a122c985594ece09bdda2591f99cc5563b
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-05 00:22:07 +0400

    read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -59,19 +59,24 @@ def main() -> None:
     """
     The main entry of the application
     """
-    logger = get_logger()
-    formatter = logging.Formatter(
-            "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: name=%(name)s;\
-                    email=%(email)s; phone=%(phone)s; ssn=%(ssn)s; \
-                    password=%(password)s; ip=%(ip)s; last_login=%(last_login)s;\
-                    user_agent=%(user_agent)s;"
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
             )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
-    csx = get_db()
-    cursor = csx.cursor()
-    cursor.execute("SELECT * from users;")
-    for row in cursor:
-        logger.info(row)
 
 
 class RedactingFormatter(logging.Formatter):
