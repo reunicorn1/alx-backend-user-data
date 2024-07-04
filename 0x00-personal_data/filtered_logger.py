38a850da954471151d5641bcab21f59cc2fbfc77
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-04 15:00:30 +0400

    troubleshoot a long docstring

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,14 @@ from typing import List
 
 
 def filter_datum(fields: List, redaction: str, message: str, seperator: str):
-    """This function returns the log message obfuscated"""
+    """
+    This function returns the log message obfuscated
+    a
+    very
+    long
+    docstring
+    """
     for field in fields:
-        regex = '(?<={}=)(.*?)(?={})'.format(field, seperator)
-        message = re.sub(regex, redaction, message)
-    retrn message
+         regex = '(?<={}=)(.*?)(?={})'.format(field, seperator)
+         message = re.sub(regex, redaction, message)
+     return messagere.sub(pattern(fields, seperator), r'\1' + redaction, message)
