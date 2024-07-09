39d3d3a0aaa1f141fdb5db4fd62bb6c02314c680
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-09 18:56:48 +0400

    basic auth class foundation set

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    This class inherits from Auth and applies the technique of
+    Basic Authentication to authorize the credentials of a user
+    """
+    pass
