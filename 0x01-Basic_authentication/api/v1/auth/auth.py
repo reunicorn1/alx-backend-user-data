e5136be760074d76ea3dab144a8417e40ba83c43
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-09 17:23:49 +0400

    completing the method require_auth which defines routes that don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,9 +26,13 @@ class Auth:
             excluded_paths: list[str]
 
         Returns:
-            False
+            bool
         """
-        return False
+        if not path or not excluded_paths or not len(excluded_paths):
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
