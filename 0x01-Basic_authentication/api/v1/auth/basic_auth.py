30a480a2cd32917fc5d759bc9eef641b94985f07
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-09 19:07:02 +0400

    extracting the base65 part out of the auth token

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,21 @@ class BasicAuth(Auth):
     This class inherits from Auth and applies the technique of
     Basic Authentication to authorize the credentials of a user
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        This method returns the Base64 part of the authorization header
+        for a Basic Authentication
+
+        Parameters:
+        authorization_header: str
+            the string representation of the encoded auth token
+
+        Returns:
+            the ASCII decoded string of the token
+        """
+        if (not authorization_header or
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith('Basic ')):
+            return None
+        return authorization_header[len('Basic '):]
