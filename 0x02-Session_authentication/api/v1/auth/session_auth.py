15ed0e06b766ec9f14c1df987747a65a6ac1f36b
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-11 14:32:16 +0400

    creating a session object and an id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Session Auth Object
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -11,4 +12,14 @@ class SessionAuth(Auth):
     authenticate his access to the server to get authorized to access
     resources available
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        This method creates a session_id for a user_id
+        """
+        if not user_id or not isinstance(user_id, str):
+            return None
+        _id = str(uuid.uuid4())
+        self.user_id_by_session_id[_id] = user_id
+        return _id
