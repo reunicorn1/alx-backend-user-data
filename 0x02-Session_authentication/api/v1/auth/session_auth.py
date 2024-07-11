c01e33c67ee1120b6a475c66aa4db609fc3606d5
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-11 23:36:36 +0400

    using session id for identifying a user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Session Auth Object
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -47,3 +48,19 @@ class SessionAuth(Auth):
         if not session_id or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        This method returns the current user instances out of the
+        request during session authentication
+
+        Attributes:
+        request: flask request object
+
+        Returns:
+            user instance
+        """
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        if user_id:
+            return User.get(user_id)
+        return None
