6eeccd1752ae323e7e59d21b14c0310d7385d76d
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-11 14:09:58 +0400

    et moi et moi et moi, using authentication to retrieve data

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Managing the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    This class manages the API authentiation of users trying to access
+    the api server
+
+    Methods:
+        require_auth
+        authorization_header
+        current_user
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        This function specifies certain endpoints of the api which require
+        authentiation
+
+        Attributes:
+            path: str
+            excluded_paths: list[str]
+
+        Returns:
+            bool
+        """
+        if not path or not excluded_paths or not len(excluded_paths):
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        if any(path.startswith(p[:-1]) for p in excluded_paths
+               if p.endswith('*')):
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        This function will be used to retrieve the authorization key from
+        the header of the request
+
+        Attributes:
+            request: Flask request object
+
+        Returns:
+            None
+        """
+        if not request or not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        This function will be used the current user based on his
+        authorization credentials
+
+        Attributes:
+            request: Flask request object
+
+        Returns:
+            None
+        """
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+import base64
+import binascii
+
+
+class BasicAuth(Auth):
+    """
+    This class inherits from Auth and applies the technique of
+    Basic Authentication to authorize the credentials of a user
+    """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        This method returns the Base64 part of the authorization header
+        for a Basic Authentication
+
+        Parameters:
+        authorization_header: str
+            the string representation of the encoded auth token
+
+        Returns:
+            the ASCII decoded string of the token
+        """
+        if (not authorization_header or
+                not isinstance(authorization_header, str) or
+                not authorization_header.startswith('Basic ')):
+            return None
+        return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """
+        This method returns the decoded value of a Base64 string
+
+        Parameters:
+        base64_authorization_header: str
+            the base64 part of the authorization header
+
+        Returns:
+            the decoded value of a Base64 string
+        """
+        if (not base64_authorization_header or
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            d = base64.b64decode(base64_authorization_header).decode('utf-8')
+        except UnicodeDecodeError:
+            return None
+        return d
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        This method returns the user email and password from the Base64
+        decoded value
+
+        Parameters:
+        decoded_base64_authorization_header: str
+            the string of username, password which is the decoded token
+
+        Returns:
+            A tuple of the user email and password
+        """
+        if (not decoded_base64_authorization_header or
+                not isinstance(decoded_base64_authorization_header, str) or
+                ':' not in decoded_base64_authorization_header):
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """
+        This method returns a User instance based on his email and
+        password
+
+        Parameters:
+        user_email: str
+        user_pwd: str
+
+        Returns:
+            The user instance based on his credentials
+        """
+        if (not user_email or
+                not isinstance(user_email, str) or
+                not user_pwd or
+                not isinstance(user_pwd, str)):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not len(users) or not users[0].is_valid_password(user_pwd):
+                return None
+            return users[0]
+        except KeyError:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        This function retrieves the current_user using the request
+
+        Parameters:
+            request: Flask request object
+
+        Returns:
+            A user instance
+        """
+        authheader = self.authorization_header(request)
+        credentials = self.decode_base64_authorization_header(
+                self.extract_base64_authorization_header(authheader))
+        email, pwd = self.extract_user_credentials(credentials)
+        return self.user_object_from_credentials(email, pwd)
