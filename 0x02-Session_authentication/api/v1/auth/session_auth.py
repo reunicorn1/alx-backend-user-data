4a471b720109286320396c0bec1cf834a17688e0
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-11 14:20:34 +0400

    created a class for the session auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Session Auth Object
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    This class is the session object which represent how the user
+    authenticate his access to the server to get authorized to access
+    resources available
+    """
+    pass
