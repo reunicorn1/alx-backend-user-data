8bafaf0e18011e922e3bf46866c016595ff7ea04
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-17 22:48:02 +0400

    db.py updating user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
@@ -51,3 +51,21 @@ class DB:
         if not result:
             raise NoResultFound
         return result
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Finds a user and then updates his attributes
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+        except InvalidRequestError or NoResultFound:
+            return
+
+        attrs = {"id": int, "email": str, "hashed_password": str,
+                 "session_id": str, "reset_token": str}
+        for k, v in kwargs.items():
+            if hasattr(User, k) and attrs[k] == type(v):
+                setattr(user, k, v)
+            else:
+                raise ValueError
+
+        self._session.commit()
