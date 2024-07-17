288bd78ec2090cd3c7a172e6b7e339bd4ed88533
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-17 22:16:41 +0400

    db.py using troubleshoot removing raise of invalidrequest

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,12 +43,11 @@ class DB:
 
     def find_user_by(self, **kwargs: str) -> User:
         """Finds a user based on keyworded attributes
-
+        """
         for k in kwargs:
             if not hasattr(User, k):
-                raise InvalidRequestError
-        """
+                raise InvalidRequestError()
         result = self._session.query(User).filter_by(**kwargs).first()
         if not result:
-            raise NoResultFound
+            raise NoResultFound()
         return result
