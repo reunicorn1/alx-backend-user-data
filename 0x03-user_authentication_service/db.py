04b08020c380ea47a8ee8044aff95162d609de8d
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-17 21:40:52 +0400

    db.py added try and exception

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,13 +31,13 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database
+        """Adds a new user to the database.
         """
         try:
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            user = None
-        return user
+            new_user = None
+        return new_user
