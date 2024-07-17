6c1b86bd63f6d6e4c38d387d7018dda0b044c6e3
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-17 21:31:20 +0400

    db.py addeda doc string for the add_user funciton

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,6 +31,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -14,3 +14,5 @@ print(user_1.id)
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
 print(user_2.id)
 
+user_2 = my_db.add_user(None, None)
+print(user_2.id)
