57d378a891d985dc5c78f0cdba316f81bb311779
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-18 11:06:48 +0400

    auth.py - destroy session super explicit??

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,8 +74,8 @@ class Auth:
                 return None
 
     def destroy_session(self, user_id: int) -> None:
-        """This method destroys a session for an active user
+        """Destroys a session associated with a given user.
         """
-        if not user_id:
+        if user_id is None:
             return None
         self._db.update_user(user_id, session_id=None)
