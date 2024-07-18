bb964e01728bc2d8f7aaa04f5129eb3812f8621c
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2024-07-18 11:58:33 +0400

    auth.py generate reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,14 @@ class Auth:
         if user_id is None:
             return None
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """This method resets the access token when user forgets it
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
